b623a5660f0b32b6d8bd564e13822cbc35dd7293
danieltorres4 <dst990502@outlook.com>
2022-03-15 18:49:15 -0600

    Documenting the code

--- similarNumbersInList.py
+++ similarNumbersInList.py
@@ -1,6 +1,12 @@
+#Sanchez Torres Sergio Daniel
+#Distributed Systems - Photon
+
+#Function that receives 2 lists of integers and prints a list with 
+#the numbers in common between the 2 received lists
 def intersection(l1, l2):
     intersection = []
 
+    #Loop to identify numbers in common
     for numbers1 in l1:
         for numbers2 in l2:
             if numbers1 == numbers2 and numbers1 not in intersection:
@@ -8,7 +14,9 @@ def intersection(l1, l2):
     
     print(intersection)
 
+#Hardcoding the 2 lists
 list1 = [2,3,5,6,11]
 list2 = [1,2,4,6,7,11]
 
+#Calling the function
 intersection(list1,list2)
